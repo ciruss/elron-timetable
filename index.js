41eba982dd68e4d0dbc133f1f09d96d34183eafb
Johan Laas <johanlaas4@gmail.com>
2023-07-08 20:37:20 +0300

    Add date selection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
+import inquirerDate from 'inquirer-date-prompt';
 import inquirerPrompt from "inquirer-autocomplete-prompt";
-import { formatDateTime, ticketLink } from "./utils.js";
+import { formatDateToTime, getDateFromDateTimeString } from "./utils.js";
 
 const bottomBar = new inquirer.ui.BottomBar();
 
@@ -21,8 +22,16 @@ const bottomBar = new inquirer.ui.BottomBar();
   };
 
   inquirer.registerPrompt('timeTable', inquirerPrompt);
+  inquirer.registerPrompt('dateForTrip', inquirerDate);
   inquirer
     .prompt([
+      {
+        type: 'dateForTrip',
+        name: 'tripDate',
+        message: 'Select the date for the trip',
+        locale: 'en-GB',
+        format: { day: 'numeric', month: 'long', year: undefined, hour: undefined, minute: undefined },
+      },
       {
         type: 'timeTable',
         name: 'startStation',
@@ -39,16 +48,21 @@ const bottomBar = new inquirer.ui.BottomBar();
     .then(async (answers) => {
       try {
         bottomBar.updateBottomBar('Loading trips...');
-        const { startStation, endStation } = answers;
+        const { startStation, endStation, tripDate } = answers;
 
         // today's date in format YYYY-MM-DD
         const today = new Date().toISOString().slice(0, 10);
+        const formatedTripDate = getDateFromDateTimeString(tripDate);
         const startStationId = stationsJson.find((station) => station.stop_name === startStation).stop_area_id;
         const endStationId = stationsJson.find((station) => station.stop_name === endStation).stop_area_id;
 
+        const tripDateObject = new Date(tripDate);
+        const tripDay = tripDateObject.getDate();
+        const tripMonth = tripDateObject.toLocaleString('default', { month: 'long' });
+        
         const requestBody = {
           channel: 'web',
-          date: today,
+          date: formatedTripDate,
           origin_stop_area_id: startStationId,
           destination_stop_area_id: endStationId,
         }
@@ -69,18 +83,24 @@ const bottomBar = new inquirer.ui.BottomBar();
           return;
         }
 
-        console.log(chalk.green(`${startStation} - ${endStation} --- price ${tripsJson.journeys[0].trips[0].product.price} \n`));
+        console.log(chalk.green(`${startStation} - ${endStation} on ${tripMonth} ${tripDay} --- price ${tripsJson.journeys[0].trips[0].product.price} \n`));
 
-        console.log(chalk.underline(ticketLink(startStation, endStation)),'\n');
+        console.log(chalk.underline(`https://elron.pilet.ee/et/otsing/${startStation}/${endStation}/${formatedTripDate}`),'\n');
 
-        tripsJson.journeys.forEach((trip) => {
-          const currentTime = formatDateTime(new Date());
-          const departureTime = formatDateTime(trip.trips[0].departure_time);
-          const arrivalTime = formatDateTime(trip.trips[0].arrival_time);
+        tripsJson.journeys.filter(trip => {
+          if (today !== formatedTripDate) return true;
 
-          if (currentTime < departureTime) {
-            console.log(`Departure ${departureTime} arrival ${arrivalTime}`);
-          }
+          const currentTime = formatDateToTime(new Date());
+          const departureTime = formatDateToTime(trip.trips[0].departure_time);
+
+          if (currentTime < departureTime) return true;
+
+          return false;
+        }).forEach((trip) => {
+          const departureTime = formatDateToTime(trip.trips[0].departure_time);
+          const arrivalTime = formatDateToTime(trip.trips[0].arrival_time);
+
+          console.log(`Departure ${departureTime} arrival ${arrivalTime}`);
         });
       } catch (error) {
         console.log(error);
--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 // format date from '2023-06-04T07:35:00.000+03:00' to '07:35'
-export const formatDateTime = (date) => {
+export const formatDateToTime = (date) => {
   const dateObject = new Date(date);
   const hours = dateObject.getHours().toString().padStart(2, '0');
   const minutes = dateObject.getMinutes().toString().padStart(2, '0');
@@ -7,7 +7,7 @@ export const formatDateTime = (date) => {
   return `${hours}:${minutes}`;
 };
 
-export const ticketLink = (startStation, endStation) => {
-  const today = new Date().toISOString().slice(0, 10);
-  return `https://elron.pilet.ee/et/otsing/${startStation}/${endStation}/${today}`;
+export const getDateFromDateTimeString = (dateTimeString) => {
+  const date = new Date(dateTimeString).toISOString().slice(0, 10);
+  return date;
 }
